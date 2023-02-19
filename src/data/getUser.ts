52c45f409fd4e321d4ed44cb58f6c3fcacadc770
s.batalov <s.batalov@kokoc.tech>
2023-02-19 18:03:29 +0300

    Промежуточный этап

--- src/data/getUser.ts
+++ src/data/getUser.ts
@@ -1,20 +1,20 @@
 import { useContext, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { Context } from "../index";
 import MessageIprops from "../types/message";
 
-export const GetUser = () => {
+export const GetUser = (user: string, pass: string) => {
   const [messagesList, setMessagesList] = useState<MessageIprops[]>([]);
   const { db } = useContext(Context);
-
-  const querySnapshot = getDocs(collection(db, "Users"));
+  const q = query(collection(db, "Users"), where("user", "==", user));
+  const querySnapshot = getDocs(q);
   querySnapshot.then((doc: any) => {
     let messages: MessageIprops[] = [];
     doc.forEach((element: any) => {
       messages.push(element.data());
     });
     setMessagesList(messages);
-    console.log(messages);    
+    console.log(messages);
   });
 
   return messagesList;
